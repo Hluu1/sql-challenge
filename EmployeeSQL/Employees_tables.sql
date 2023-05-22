7236f0440f28ed01651fd9fe99088c1cb881dd5d
Hieu <Hieu.luu1@Hotmail.com>
2023-05-22 16:53:49 +0800

    Made all tables and imported necessary files for each of the six excel files.

---
+++ EmployeeSQL/Employees_tables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE departments (
+  dept_no CHAR(4) PRIMARY KEY,
+  dept_name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no CHAR(4) NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_manager (
+  dept_no CHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE employees (
+  emp_no INT PRIMARY KEY,
+  emp_title_id VARCHAR(10) NOT NULL,
+  birth_date DATE NOT NULL,
+  first_name VARCHAR(14) NOT NULL,
+  last_name VARCHAR(16) NOT NULL,
+  sex VARCHAR(1) CHECK (sex IN ('M', 'F')) NOT NULL,
+  hire_date DATE NOT NULL,
+  FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  title_id VARCHAR(10) PRIMARY KEY,
+  title VARCHAR(50) NOT NULL
+);
